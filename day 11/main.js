e7e9e4bb658244a2fe8eea5525d428f4a8970d6a
ShakibRamez <shakibramiz1@icloud.com>
2025-06-19 00:55:06 +0430

    for each loop with arrow function and map array method completed

--- day 11/main.js
+++ day 11/main.js
@@ -1,1 +1,47 @@
-const number =[1 ,2 ,3 ,4 , 5];
+// for each method with arrow function
+// const numbers = [1, 2, 3, 4, 5];
+
+// numbers.forEach((num) => {
+//   console.log(num * 2);
+// });
+
+//sum function 
+
+// function sum(numbers) {
+//   let total = 0;
+//   numbers.forEach(num => {
+//     total += num;
+//   });
+//   return total;
+// }
+
+// const myNumbers = [1, 2, 3, 4, 5];
+// const result = sum(myNumbers);
+// console.log(result); 
+
+
+//map array method
+const products = [
+  {
+    name: 'laptop',
+    price: 1000,
+    count: 5
+  },
+  {
+    name: 'desktop',
+    price: 1500,
+    count: 2
+  },
+  {
+    name: 'phone',
+    price: 500,
+    count: 10
+  }
+];
+
+const totalProductsValue = products.map(item => ({
+  name: item.name,
+  totalValue: item.price * item.count
+}));
+
+console.log(totalProductsValue);
