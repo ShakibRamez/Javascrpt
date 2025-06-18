57e0c55f5a734d33b02585aaff14ac4830775d2f
ShakibRamez <shakibramiz1@icloud.com>
2025-06-19 01:18:41 +0430

    filter method and reduce array method completed

--- day 11/main.js
+++ day 11/main.js
@@ -21,6 +21,73 @@
 
 
 //map array method
+// const products = [
+//   {
+//     name: 'laptop',
+//     price: 1000,
+//     count: 5
+//   },
+//   {
+//     name: 'desktop',
+//     price: 1500,
+//     count: 2
+//   },
+//   {
+//     name: 'phone',
+//     price: 500,
+//     count: 10
+//   }
+// ];
+
+// const totalProductsValue = products.map(item => ({
+//   name: item.name,
+//   totalValue: item.price * item.count
+// }));
+
+// console.log(totalProductsValue);
+
+
+//filter method
+// const products = [
+//   {
+//     name: 'laptop',
+//     price: 1000,
+//     count: 5
+//   },
+//   {
+//     name: 'desktop',
+//     price: 1500,
+//     count: 2
+//   },
+//   {
+//     name: 'phone',
+//     price: 500,
+//     count: 10
+//   }
+// ];
+
+// const expensiveProducts = products.filter(item => item.price > 1000);
+
+// console.log(expensiveProducts);
+
+//reduce array method
+
+//ex1
+const numbers =[10, 2, 3, 4, 5, 6, 7];
+
+const max = numbers.reduce(callback, -Infinity);
+
+function callback(accumullattor, value){
+    if(accumullattor > value){
+        return accumullattor;
+    }else{
+        return value;
+    }
+}
+
+console.log(max);
+
+//ex2
 const products = [
   {
     name: 'laptop',
@@ -39,9 +106,9 @@ const products = [
   }
 ];
 
-const totalProductsValue = products.map(item => ({
-  name: item.name,
-  totalValue: item.price * item.count
-}));
+const totalValueStore = products.reduce(
+(acc, item) => + item.value * item.count,
+0
+);
 
-console.log(totalProductsValue);
+console.log(totalValueStore);
