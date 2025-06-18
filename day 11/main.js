b008773a77732a314fbaffb6377849cc8b200e05
ShakibRamez <shakibramiz1@icloud.com>
2025-06-19 02:15:09 +0430

    slice, splice sort, contact, fill, includes, join reverse, push, pop, unshift, shift, index of, every, some method array worked

--- day 11/main.js
+++ day 11/main.js
@@ -73,42 +73,120 @@
 //reduce array method
 
 //ex1
-const numbers =[10, 2, 3, 4, 5, 6, 7];
+// const numbers =[10, 2, 3, 4, 5, 6, 7];
 
-const max = numbers.reduce(callback, -Infinity);
+// const max = numbers.reduce(callback, -Infinity);
 
-function callback(accumullattor, value){
-    if(accumullattor > value){
-        return accumullattor;
-    }else{
-        return value;
-    }
-}
+// function callback(accumullattor, value){
+//     if(accumullattor > value){
+//         return accumullattor;
+//     }else{
+//         return value;
+//     }
+// }
 
-console.log(max);
+// console.log(max);
 
-//ex2
-const products = [
-  {
-    name: 'laptop',
-    price: 1000,
-    count: 5
-  },
-  {
-    name: 'desktop',
-    price: 1500,
-    count: 2
-  },
-  {
-    name: 'phone',
-    price: 500,
-    count: 10
-  }
-];
+// //ex2
+// const products = [
+//   {
+//     name: 'laptop',
+//     price: 1000,
+//     count: 5
+//   },
+//   {
+//     name: 'desktop',
+//     price: 1500,
+//     count: 2
+//   },
+//   {
+//     name: 'phone',
+//     price: 500,
+//     count: 10
+//   }
+// ];
 
-const totalValueStore = products.reduce(
-(acc, item) => + item.value * item.count,
-0
-);
+// const totalValueStore = products.reduce(
+// (acc, item) => + item.value * item.count,
+// 0
+// );
 
-console.log(totalValueStore);
+// console.log(totalValueStore);
+
+//slice array method 
+
+// const fruits = ['apple', 'banana', 'cherry', 'date'];
+// const citrus = fruits.slice(1, 3);
+
+// console.log(citrus);
+
+
+//splice method array 
+// const numbers = [1, 2, 3, 4, 5];
+// const deleted = numbers.splice(2, 0, 6, 7, 20, 100);
+
+// console.log(numbers);
+// console.log(deleted);
+
+//sort method array
+// const nums = [40, 100, 1, 5, 25];
+// nums.sort((a, b) => a - b);
+// console.log(nums);
+
+// //contact method array
+// const a = [1, 2];
+// const b = [3, 4];
+// const c = a.concat(b);
+// console.log(c);
+
+//fill method array
+// const arr = [1, 2, 3, 4];
+// arr.fill(0, 1, 3);
+// console.log(arr);
+
+//includes method array
+
+// const fruits = ['apple', 'banana'];
+// console.log(fruits.includes('banana'));
+
+//join method array 
+// const words = ['Hello', 'World'];
+// console.log(words.join(' ')); 
+
+//reverse method array
+// const arr = [1, 2, 3];
+// arr.reverse();
+// console.log(arr); 
+
+//push method array
+// const arr = [1, 2];
+// arr.push(3);
+// console.log(arr);
+
+//pop method array
+// const arr = [1, 2, 3];
+// arr.pop();
+// console.log(arr);
+
+
+// //unshift method array
+// const arr = [2, 3];
+// arr.unshift(1);
+// console.log(arr); 
+
+// //shift method array
+// const arr = [1, 2, 3];
+// arr.shift();
+// console.log(arr);
+
+// //index of method array
+// const arr = ['a', 'b', 'c'];
+// console.log(arr.indexOf('b'));
+
+// //every method array
+// const ages = [22, 30, 25];
+// console.log(ages.every(age => age > 18));
+
+//some method array
+const ages = [16, 21, 15];
+console.log(ages.some(age => age >= 18)); 
